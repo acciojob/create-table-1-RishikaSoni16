ec31cf5036550a85e5e9c0605d1c7944e2c01396
RishikaSoni16 <135811075+RishikaSoni16@users.noreply.github.com>
2024-05-09 16:03:13 +0600

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    let t1 = document.getElementById("sampleTable");
+	 var newRow = t1.insertRow(0);
+	newRow.innerHTML += `<tr><td>New Cell1</td> 
+		<td>New Cell2</td></tr> `;
 }
